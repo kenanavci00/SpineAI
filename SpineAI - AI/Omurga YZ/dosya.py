fd5631c2cf8237e22cf6254e154198acb6770d59
Kenan Avcı <kenanavc44@gmail.com>
2026-03-23 12:03:42 +0300

    Update dosya.py

--- SpineAI - AI/Omurga YZ/dosya.py
+++ SpineAI - AI/Omurga YZ/dosya.py
@@ -2,11 +2,7 @@ import os
 
 
 def etiketleri_sifirla(klasor_yolu):
-    """
-    Bir klasördeki tüm .txt dosyalarını okur.
-    Satırın başındaki sınıf numarası ne olursa olsun (0, 1, 2, 3, 4...)
-    hepsini '0' (Vertebrae) yapar.
-    """
+
     dosyalar = [f for f in os.listdir(klasor_yolu) if f.endswith('.txt')]
     degisen_dosya = 0
 
@@ -20,11 +16,11 @@ def etiketleri_sifirla(klasor_yolu):
         for satir in satirlar:
             parcalar = satir.strip().split()
             if len(parcalar) > 0:
-                # İLK NUMARAYI ZORLA '0' YAP (HEPSİ ARTIK OMURGA)
+
                 parcalar[0] = "0"
                 yeni_satirlar.append(" ".join(parcalar) + "\n")
 
-        # Dosyayı üzerine yaz
+      
         with open(dosya_path, 'w') as f:
             f.writelines(yeni_satirlar)
             degisen_dosya += 1
@@ -32,9 +28,6 @@ def etiketleri_sifirla(klasor_yolu):
     print(f"Bitti! {klasor_yolu} içindeki {degisen_dosya} dosyanın tüm sınıfları '0' yapıldı.")
 
 
-# --- BURAYI DÜZENLE ---
-# İndirdiğin "lumbar_spondylolisthesis" (5 sınıflı olan) klasörün yollarını yaz
-# Başına 'r' koymayı unutma.
 
 print("Train etiketleri düzeltiliyor...")
 etiketleri_sifirla(r"C:\Users\kenan\Desktop\Multimodel\train\labels")
